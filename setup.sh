c1db36ff2892255eccf98e763a87d90fdfdb4649
kerongonz <kerongonz@gmail.com>
2024-02-18 02:10:12 +0000

    update setup

--- setup.sh
+++ setup.sh
@@ -12,4 +12,5 @@ cp $(find /nix/store -name 'php.ini' -path '*php-8.1.27/etc*' -print) php_config
 # Find and copy the extensions.ini file
 cp $(find /nix/store -name 'php.ini' -path '*php-with-extensions-8.1.27/lib*' -print) php_config/extensions.ini
 
+# Increase max_input_vars value for Moodle
 sed -i 's/;max_input_vars = 1000/max_input_vars = 6000/' php_config/php.ini
